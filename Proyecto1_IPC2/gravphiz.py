51cd11c3a2674150d8b7c70d30067adb34cba1b7
Kevyn <3003540960101@ingenieria.usac.edu.gt>
2021-03-11 01:53:07 -0600

    ya lo arregle

--- Proyecto1_IPC2/gravphiz.py
+++ Proyecto1_IPC2/gravphiz.py
@@ -8,26 +8,37 @@ class gravphiz():
         ene = '"n = ' + n + '"'
         head = 'digraph mat{\nrankdir=UD\n\tMatrices->' + name + '\n'
         body = '\t' + name + '->' + eme + '\n\t' + name + '->' + ene + '\n'
+        declarar_nodos = ""
         contador = 0
         body_aux = ""
         list_aux = []
+        ayuda = []
         code = ""
         #str(a._datos__dato)
+        cont = 0
+        for a in list:
+            declarar_nodos = declarar_nodos + '"n' + str(cont) + '" [label = "' + str(a._datos__dato) + '"]\n'
+            cont = cont + 1
 
+        #for a in ayuda:
+         #   print(a)
+
+        cont_aux = 0
         for a in list:
             if contador < int(m):
-                body_aux = body_aux + '->' + str(a._datos__dato)
+                body_aux = body_aux + '->n' + str(cont_aux) #str(a._datos__dato)
                 contador = contador + 1
             #print(a._datos__dato)
             if contador == int(m):
                 list_aux.append(body_aux)
                 body_aux = ""
                 contador = 0
+            cont_aux = cont_aux + 1
         #print(list_aux)
         for a in list_aux:
             body_aux = body_aux + '\t' + name + a + '\n'
             
-        code = head + body + body_aux + '\n}'
+        code = head + declarar_nodos + body + body_aux + '\n}'
         #print(code)
         return code
 
